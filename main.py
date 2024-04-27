15a56eb45b6237cd2afd91734e8ff5dc7866d53f
Yusuf <87618237+CatLover248@users.noreply.github.com>
2024-04-27 21:31:39 +0200

    implemented some basic stuff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify,make_response
+from flask import Flask,jsonify,make_response,request
 
 app = Flask(__name__)
 
@@ -29,7 +29,7 @@ def document_new():
     return make_response(jsonify(response_data), 200)
 
 @app.route("/document/read/<int:id>")
-def document_id(id):
+def document_read_id(id):
     if id > (len(documents)-1):
         response_data = {"status":"ERROR_ID_OUT_OF_BOUNDS"}
         return make_response(jsonify(response_data), 400)
@@ -42,20 +42,30 @@ def document_id(id):
     response_data = {"status":"ERROR_DOCUMENT_NOT_FOUND_BY_ID"}
     return make_response(jsonify(response_data), 400)
 
-@app.route("/document/write/<int:id>")
-def document_id(id):
-    if id > (len(documents)-1):
-        response_data = {"status":"ERROR_ID_OUT_OF_BOUNDS"}
+@app.route("/document/write/<int:id>", methods=["GET", "POST"])
+def document_write_id(id):
+    if request.is_json:
+        data = request.json
+        try:
+            content = data.get("content")
+        except:
+            response_data = {"status":"ERROR_CONTENT_MISSING_JSON"}
+            return make_response(jsonify(response_data), 400)
+        if id > (len(documents)-1):
+            response_data = {"status":"ERROR_ID_OUT_OF_BOUNDS"}
+            return make_response(jsonify(response_data), 400)
+        
+        for document in documents:
+            if document.id == id:
+                document.content = str(content)
+                response_data = {"status":"SUCCESS_CONTENT_MODIFIED"}
+                return make_response(jsonify(response_data), 200)
+
+        response_data = {"status":"ERROR_DOCUMENT_NOT_FOUND_BY_ID"}
         return make_response(jsonify(response_data), 400)
-    
-    for document in documents:
-        if document.id == id:
-            response_data = {"content":str(document.content), "status":"SUCCESS_CONTENT_ACCESSED"}
-            return make_response(jsonify(response_data), 200)
-
-    response_data = {"status":"ERROR_DOCUMENT_NOT_FOUND_BY_ID"}
-    return make_response(jsonify(response_data), 400)
-
+    else:
+        response_data = {"status":"ERROR_JSON_DATA_MISSING"}
+        return make_response(jsonify(response_data), 400)
 
 app.run(port=80, debug=True)
 
